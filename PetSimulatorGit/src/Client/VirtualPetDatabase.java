151723d5b92b7fefb3c2f5ebd907bd652f50841c
Oscar <Oscar@192.168.1.18>
2020-10-30 16:42:21 +1300

    database stuff should be complete

--- PetSimulatorGit/src/Client/VirtualPetDatabase.java
+++ PetSimulatorGit/src/Client/VirtualPetDatabase.java
@@ -6,6 +6,7 @@
 package Client;
 
 import cats.Cat;
+import cats.CatFactory;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -14,7 +15,10 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.sql.Blob;
+import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 
 /**
@@ -22,7 +26,9 @@ import java.sql.PreparedStatement;
  * @author Oscar //LOGIN: OscarAshley AshleyOscar
  *
  * This class is a singleton, with private constructor, reference to self, and
- * getInstance method.
+ * getInstance method. Contains public methods to get pet by id, get list of
+ * pets in database (id and name, not Cat objects), save pet to database,
+ *
  */
 public class VirtualPetDatabase {
 
@@ -33,7 +39,8 @@ public class VirtualPetDatabase {
     private Connection conn;
 
     private VirtualPetDatabase() {
-
+        establishConnection();
+        createPetTable();
     }
 
     //Synchronized inside the method so that threads do not get delayed as much
@@ -48,52 +55,85 @@ public class VirtualPetDatabase {
         return instance;
     }
 
-    //DROPS IF EXISTS WE DO NOT WANT THIS
     //save order: id, name, age, serializable
-    public void createPetTable() {
+    //creates the table if it doesn't exist with values for id (identity), name, age, serial (stored as blob)
+    private void createPetTable() {
         try {
             Statement statement = conn.createStatement();
-            String newTable = "pettable";
+            String newTable = "PETS";
 
-            statement.executeUpdate("drop table if exists" + newTable);
-
-            String sqlCreateTable = "CREATE TABLE" + newTable + " (ID INT NOT NULL, NAME VHARCHAR(50),"
-                    + "AGE INT, SERIAL BLOB(512), PRIMARY KEY (ID))";
-
-            statement.executeUpdate(sqlCreateTable);
+//            statement.executeUpdate("drop table " + newTable);
+            DatabaseMetaData dbmd = conn.getMetaData();
+            ResultSet rs = dbmd.getTables(null, null, newTable, null);
+            if (!rs.next()) {
+                String sqlCreateTable = "CREATE TABLE " + newTable + " (ID INT GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), Name VARCHAR(50),"
+                        + " Age INT, Serial BLOB(512))";
+                statement.executeUpdate(sqlCreateTable);
+            }
         } catch (SQLException ex) {
             System.err.println("SQLException: " + ex.getMessage());
         }
     }
 
+    //return a list of pet id's + names + ages of all pets in the database.
     public String[] getPets() {
         ResultSet rs = null;
         ArrayList<String> savedCats = new ArrayList<>();
-        
-        try{
+
+        try {
             Statement statement = conn.createStatement();
-            String sqlQuery = "select * from pettable";
+            String sqlQuery = "select * from PETS";
             rs = statement.executeQuery(sqlQuery);
-            if (rs == null){
+            if (rs.next()) {
+                do {
+                    String temp = "id: " + rs.getInt("ID") + ", name: " + rs.getString("Name") + ", age: " + rs.getInt("Age");
+                    savedCats.add(temp);
+                } while (rs.next());
+            } else {
                 System.out.println("result set null, no pets saved.");
                 return null;
             }
-            do{
-                String temp = "id: " + rs.getInt("ID") + ", name: " + rs.getString("NAME") + ", age: " + rs.getInt("AGE");
-                savedCats.add(temp);
-            }while (rs.next());
-            return (String[]) savedCats.toArray();
-        }catch(SQLException ex){
+            return savedCats.toArray(new String[0]);
+        } catch (SQLException ex) {
             System.err.println("SQLException: " + ex.getMessage());
             return null;
         }
     }
-    
-    public boolean insertPet(Cat c){
-        String query = "INSERT INTO pettable VALUES(?, ?, ?)";
+
+    //return null if id did not return anything
+    //gets cat object by id in database, using serial value and object reading/writing
+    public Cat getPetByID(int ID) {
+        ResultSet rs = null;
+        Cat result = null;
+        try {
+            Statement statement = conn.createStatement();
+            String sqlQuery = "select serial from PETS where ID = " + ID;
+            rs = statement.executeQuery(sqlQuery);
+            if (rs.next()) {
+                Blob temp = rs.getBlob("Serial");
+                try {
+                    ObjectInputStream ois = new ObjectInputStream(temp.getBinaryStream());
+                    result = (Cat) ois.readObject();
+                } catch (IOException | ClassNotFoundException ex) {
+                    System.err.println("Exception in getPetByID: " + ex.getMessage());
+                }
+
+            } else {
+                return null;
+            }
+        } catch (SQLException ex) {
+            System.err.println("SQLException: " + ex.getMessage());
+        }
+
+        return result;
+    }
+
+    //insert cat object into database. serialises the object as last value to easily retrieve.
+    public boolean insertPet(Cat c) {
+        String query = "INSERT INTO PETS VALUES(DEFAULT, ?, ?, ?)";
         byte[] data = null;
-        
-        try{
+
+        try {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             ObjectOutputStream oos = new ObjectOutputStream(baos);
             oos.writeObject(c);
@@ -101,30 +141,62 @@ public class VirtualPetDatabase {
             oos.close();
             baos.close();
             data = baos.toByteArray();
-        }catch(IOException ex){
+        } catch (IOException ex) {
             System.err.println("IOException: " + ex.getMessage());
             return false;
         }
-        
-        try{ //NAME AGE SERIAL
+
+        try { //NAME AGE SERIAL
             PreparedStatement statement = conn.prepareStatement(query);
             statement.setString(1, c.getName());
             statement.setInt(2, c.getAge());
             statement.setObject(3, data);
             statement.executeUpdate();
             return true;
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             System.err.println("SQLException: " + ex.getMessage());
             return false;
         }
     }
 
-    public void establishConnection() {
+    private void establishConnection() {
         try {
             conn = DriverManager.getConnection(URL, username, password);
             System.out.println(URL + " connected");
         } catch (SQLException ex) {
             System.err.println("SQLException: " + ex.getMessage());
+            System.err.println("Next error: " + ex.getNextException().getMessage());
+        }
+    }
+
+    //return true if connection closed otherwise false
+    public boolean closeConnection() {
+        try {
+            conn.close();
+            return true;
+        } catch (SQLException ex) {
+            System.err.println("SQLException: " + ex.getMessage());
+            return false;
+        }
+    }
+
+    //for testing/debugging purposes.
+    public static void main(String[] args) {
+        VirtualPetDatabase vpdb = VirtualPetDatabase.getInstance();
+
+        System.out.println("Insert pet: " + vpdb.insertPet(CatFactory.getCat()));
+        System.out.println("getting pets...");
+        String[] pets = vpdb.getPets();
+        for (String p : pets) {
+            System.out.println(p);
+        }
+
+        Cat temp = vpdb.getPetByID(101);
+        System.out.println(temp);
+        try {
+            vpdb.conn.close();
+        } catch (SQLException ex) {
+            System.err.println("SQLException in main: " + ex.getMessage());
         }
     }
 }
